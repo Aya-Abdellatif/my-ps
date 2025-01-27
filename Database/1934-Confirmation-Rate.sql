a035540e4a1584ff1db7e1ad3ae9b33a2f595a97
Aya Abdellatif <106776143+Aya-Abdellatif@users.noreply.github.com>
2025-01-27 19:36:36 +0200

    [1934] [Time Beats: 26.55%] - LeetPush

--- Database/1934-Confirmation-Rate.sql
+++ Database/1934-Confirmation-Rate.sql
@@ -1,11 +1,15 @@
 # Write your MySQL query statement below
-
+  
+#	[1934] [Time Beats: 26.55%] - LeetPush
+  
 Select s.user_id, Round(Avg(If(c.action = 'confirmed', 1, 0)), 2) As confirmation_rate
 From Signups s left join Confirmations c on s.user_id = c.user_id
 Group By s.user_id
 
 
 /*
+#[197] [Time Beats: 74.70%] - LeetPush
+
 #COALESCE: if null returns 0
 Select s.user_id, Coalesce(Round(Sum(c.action = 'confirmed')/Count(*), 2), 0.00) As confirmation_rate
 From Signups s left join Confirmations c on s.user_id = c.user_id
