f7637d00bd3f788cba7625d9c8ae31e78a3bd549
Aya Abdellatif <106776143+Aya-Abdellatif@users.noreply.github.com>
2025-01-27 18:22:40 +0200

    [570] [Time Beats: 9.96%] - LeetPush

--- Database/570-Managers-with-at-Least-5-Direct-Reports.sql
+++ Database/570-Managers-with-at-Least-5-Direct-Reports.sql
@@ -1,5 +1,16 @@
 # Write your MySQL query statement below
+/*
+# [570] [Time Beats: 71.19%] - LeetPush
 SELECT e.name
-FROM Employee e INNER JOIN Employee m ON e.id = m.managerId
+FROM Employee e Join (
+    SELECT managerId, Count(managerId) As directReports
+    FROM Employee
+    GROUP BY managerId
+    HAVING COUNT(managerId) >= 5
+    ) e2 ON e.id = e2.managerId;
+*/
+#another solution
+SELECT E.name
+FROM Employee E INNER JOIN Employee m ON E.id = m.managerId
 GROUP BY m.managerId
 HAVING COUNT(m.managerId) >= 5;
