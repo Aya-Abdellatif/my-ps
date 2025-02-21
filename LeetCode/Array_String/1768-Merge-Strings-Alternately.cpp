69a4d3ac5dc27642bdf9d2f70902b06bad34177c
Aya Abdellatif <106776143+Aya-Abdellatif@users.noreply.github.com>
2025-02-21 18:08:37 +0200

    [1768] [Time Beats: 100.00%] [Memory Beats: 12.74%] - LeetPush

--- LeetCode/Array_String/1768-Merge-Strings-Alternately.cpp
+++ LeetCode/Array_String/1768-Merge-Strings-Alternately.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        int l1 = word1.length(), l2 = word2.length(), max_l = max(l1,l2);
-        string merged = "";
+        int l1 = word1.length(), l2 = word2.length();
+        string merged;
+        merged.reserve(l1 + l2);  // Preallocate memory
+        //If you concatenate without preallocating space, a new string is allocated when needed, 
+        //and old contents are copied over.
 
-        for(int i = 0; i < max_l; i++){
-            if(i<l1){
-                merged += word1[i];
-            }
-            if(i<l2){
-                merged += word2[i];
-            }
+        int i = 0;
+        while (i < l1 || i < l2) {
+            if (i < l1) 
+                //push_back() is more efficient for adding single characters,
+                //and only appends a single character and does not check string capacity explicitly.
+                merged.push_back(word1[i]); 
+
+            if (i < l2) 
+                merged.push_back(word2[i]);
+            
+            i++;
         }
         return merged;
     }
