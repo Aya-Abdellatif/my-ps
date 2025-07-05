acd0d05e065ff57f4b6276cdd56c3d2b02c832c7
Aya Abdellatif <106776143+Aya-Abdellatif@users.noreply.github.com>
2025-07-05 17:56:11 +0300

    [27] [Time Beats: 100.00%] [Memory Beats: 5.18%] - LeetPush

--- LeetCode/arrays-hashing/Remove-Element.cpp
+++ LeetCode/arrays-hashing/Remove-Element.cpp
@@ -5,3 +5,17 @@ public:
         return nums.size();
     }
 };
+
+/*
+ remove(v.begin(), v.end(), 20)
+remove does not actually erase elements from the vector.
+
+Instead, it rearranges the elements by moving all elements not equal to 20 to the front.
+
+It returns an iterator (new_end) that points to the new logical end of the vector (the part after that contains undefined or "garbage" values).
+
+v.erase(new_end, v.end())
+Now, this erase() call actually removes the "garbage" or duplicate elements from the vector.
+
+It erases everything from the new_end to the real v.end(), effectively shrinking the vector.
+*/
